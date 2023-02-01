bfe72ec4e2ca7318af16082ee8dab7855c69ba56
kritartha bikram shah <thisiskritartha@gmail.com>
2023-02-01 17:10:10 +0545

    Add the custom model(.tflite) for the image labeling

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'package:flutter/foundation.dart';
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
 import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 late List<CameraDescription> _camera;
 
@@ -28,10 +31,13 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
 
-    //Initialize labeler
-    final ImageLabelerOptions options =
-        ImageLabelerOptions(confidenceThreshold: 0.7);
-    imageLabeler = ImageLabeler(options: options);
+    //Initialize labeler for the base model
+    // final ImageLabelerOptions options =
+    //     ImageLabelerOptions(confidenceThreshold: 0.7);
+    // imageLabeler = ImageLabeler(options: options);
+
+    //Initialize labeler for the custom model(.tflite)
+    createImageLabeling();
 
     //Initialize controller
     controller = CameraController(_camera[0], ResolutionPreset.high);
@@ -101,18 +107,40 @@ class _MyHomePageState extends State<MyHomePage> {
     return inputImage;
   }
 
+  Future<String> _getModel(String assetPath) async {
+    if (Platform.isAndroid) {
+      return 'flutter_assets/$assetPath';
+    }
+    final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
+    await Directory(dirname(path)).create(recursive: true);
+    final file = File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(assetPath);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
+  }
+
+  createImageLabeling() async {
+    final modelPath = await _getModel('assets/ml/mobilenet.tflite');
+    final options = LocalLabelerOptions(modelPath: modelPath);
+    imageLabeler = ImageLabeler(options: options);
+  }
+
   doImageLabeling() async {
     final InputImage inputImage = getInputImage();
     final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
     result = '';
     for (ImageLabel label in labels) {
       final String text = label.label;
-      final int index = label.index;
+      //final int index = label.index;
       final String confidence = (label.confidence.toStringAsFixed(2));
       result += '$text  $confidence\n';
     }
-    isBusy = false;
+
     setState(() {
+      isBusy = false;
       result;
     });
   }
@@ -137,6 +165,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: const TextStyle(
                   fontSize: 30.0,
                   fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none,
                   color: Colors.white,
                 ),
               ),
